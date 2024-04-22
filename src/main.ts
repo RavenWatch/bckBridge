c0ee151131cedc5add7425ee809e527e8c09ec68
unknown <julioc.fraposo@gmail.com>
2024-04-22 09:10:09 -0300

    production ready

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({origin:["http://localhost:3000","fb-bridge-8jq7ex3bx-julio-raposos-projects.vercel.app"]})
+  app.enableCors({origin:["http://localhost:3000","https://fb-bridge.vercel.app"]})
   await app.listen(8000);
 }
 bootstrap();
