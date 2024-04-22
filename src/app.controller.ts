c14893430737f9e5e1e05ef6a64c5004ea309ea9
unknown <julioc.fraposo@gmail.com>
2024-04-22 09:01:02 -0300

    mb

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -4,6 +4,10 @@ import { AppService } from './app.service';
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+  @Get()
+  helloBridge(){
+    return "Hello BRIDGE!!!"
+  }
 
   @Get(':id')
   getListaOfPrimoNumbers(@Param('id') id:number) {
