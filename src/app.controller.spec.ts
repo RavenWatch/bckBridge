5678c94e9cdadbc36dcaf71e6543823123d4593c
unknown <julioc.fraposo@gmail.com>
2024-04-21 22:12:00 -0300

    update commit

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -14,9 +14,16 @@ describe('AppController', () => {
     appController = app.get<AppController>(AppController);
   });
 
-  describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+  describe('test de equivalência entre numeros e seu output', () => {
+    it('deve retornar 4 ao ser dado 10 como input', () => {
+      expect(appController.getListaOfPrimoNumbers(10).numbers).toBe(4)
+    });
+    it('deve retornar 0 ao ser dado números menores que 2 ou que não sejam inteiros como input ', () => {
+      expect(appController.getListaOfPrimoNumbers(1).numbers).toBe(-1)
+      expect(appController.getListaOfPrimoNumbers(0).numbers).toBe(-1)
+      expect(appController.getListaOfPrimoNumbers(-1).numbers).toBe(-1)
+      expect(appController.getListaOfPrimoNumbers(3.7).numbers).toBe(-1)
+
     });
   });
 });
--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,47 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
+    let vr = 1.2 // * 10 | % 10
     return 'Hello World!';
+  }
+  
+  checkNumberOfEvenNumbers(num:number){
+    let startingTime=new Date()
+    console.log( (startingTime.getSeconds() * 1000) + startingTime.getMilliseconds() )
+    console.log(startingTime)
+    let listOfPrimoNumbers=[]
+    if(
+      num < 2 
+      ||
+     ( (num * 10) % 10 ) !=0 //Testando se o número é inteiro, se não for é invalido
+    ){
+      console.log("INVALIDO")
+      return {numbers:-1,time:-1}
+    }
+    for(let numTeste=2;numTeste < num ; numTeste++){
+      if(this.isNumberPrimo(numTeste)){
+        listOfPrimoNumbers=[...listOfPrimoNumbers,numTeste]
+      }
+    }
+
+    let endingTime=new Date()
+    console.log(endingTime)
+    console.log( (endingTime.getSeconds() * 1000) + endingTime.getMilliseconds() )
+
+    let calculationTime = ((endingTime.getSeconds() * 1000) + endingTime.getMilliseconds()) -  ((startingTime.getSeconds() * 1000)+startingTime.getMilliseconds())
+    
+    console.log(calculationTime)
+    return {numbers:listOfPrimoNumbers.length,time:calculationTime} 
+  }
+
+  isNumberPrimo(num:number){
+    let numberOfDivisors=0
+    for(let ind = 2 ; ind < num ; ind ++){
+      if( (num % ind) == 0 ){
+        numberOfDivisors +=  1
+      }
+    }
+    return numberOfDivisors == 0
+     
   }
 }
